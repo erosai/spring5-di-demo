fb882be08d05f22cf23a6d56c5e85931d0edc2b0
erosai <eror.shahini@gmail.com>
2020-12-25 16:13:51 +0100

    saving

--- src/main/java/guru/springframework/controllers/MyController.java
+++ src/main/java/guru/springframework/controllers/MyController.java
@@ -1,2 +1,10 @@
-package guru.springframework.controllers;public class MyController {
+package guru.springframework.controllers;
+
+public class MyController {
+
+    public String hello(){
+        System.out.println("Hello");
+
+        return "foo";
+    }
 }
